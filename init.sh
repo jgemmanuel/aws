b8077359dfabd1f691ead80a6ca17a1acb952d07
mkota <jg.emmanuel@outlook.com>
2014-04-09 13:36:17 +0000

    bugfix `init.sh' and `backup'

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 HOME=/home/ubuntu
+EMACS=$HOME/.emacs.d
+REPO=$HOME/repos
 
 sudo locale-gen en_GB.UTF-8
 
@@ -16,6 +18,7 @@ sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
 # Install everything else
 sudo apt-get install -y git aspell r-base ess nodejs npm tree texlive auctex octave
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g express stylus jade
 
 # Setup git, dot-files and custom
@@ -23,9 +26,14 @@ git config --global push.default simple
 git config --global user.name "mkota"
 git config --global user.email jg.emmanuel@outlook.com
 
-mkdir -p ~/repos
-cd ~/repos
-git clone https://github.com/mkota/dotfiles.git
+ssh-keygen -t rsa -C "jg.emmanuel@outlook.com"
+ssh-agent /bin/bash
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub
+
+read -p "Setup github and hit ENTER"
+
+git clone git@github.com:mkota/dotfiles.git $REPO
 ln -sf ~/repos/dotfiles/emacs.d ~/.emacs.d
 ln -sf ~/repos/dotfiles/bashrc ~/.bashrc
 ln -sf ~/repos/dotfiles/bashrc_custom ~/.bashrc_custom
@@ -35,23 +43,19 @@ ln -sf ~/repos/dotfiles/octaverc ~/.octaverc
 source ~/.profile
 source ~/.bashrc
 
-cd ~/.emacs.d
-wget http://adamspiers.org/computing/elisp/smooth-scrolling.el
-wget https://raw.githubusercontent.com/winterTTr/ace-jump-mode/master/ace-jump-mode.el
-wget http://orgmode.org/org-8.2.5h.tar.gz
-tar xzvf org-8.2.5h.tar.gz
-rm -f org-8.2.5h.tar.gz
+wget http://adamspiers.org/computing/elisp/smooth-scrolling.el -P $EMACS
+wget https://raw.githubusercontent.com/winterTTr/ace-jump-mode/master/ace-jump-mode.el -P $EMACS
+wget http://orgmode.org/org-8.2.5h.tar.gz -P $EMACS
+tar xzvf $EMACS/org-8.2.5h.tar.gz
+rm -f $EMACS/org-8.2.5h.tar.gz
 
-ssh-keygen -t rsa -C "jg.emmanuel@outlook.com"
-ssh-agent /bin/bash
-ssh-add ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub
-
-read -p "Setup github and hit ENTER"
-
-cd ~/repos
-git clone git@bitbucket.org:mkota/custom.git
+git clone git@bitbucket.org:mkota/custom.git $REPO
 mkdir -p ~/texmf/tex/latex
 ln -sf ~/repos/custom/latex/*.sty ~/texmf/tex/latex/
+
+# Cron
+git clone git@github.org:mkota/aws.git $REPO
+sudo cp $REPO/aws/backup /etc/cron.hourly/
+sudo chmod +x /etc/cron.hourly/backup
 
 echo "Init done."
